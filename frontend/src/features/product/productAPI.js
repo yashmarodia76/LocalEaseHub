8aad99ccb9f9df2d8c6ab8827f4b09ed543db42d
Yash Marodia <yashmarodia76@gmail.com>
2024-01-03 00:44:44 +0530

    deploy

--- frontend/src/features/product/productAPI.js
+++ frontend/src/features/product/productAPI.js
@@ -2,7 +2,7 @@
 export function fetchProductById(id) {
   return new Promise(async (resolve) => {
     //TODO: we will not hard-code server URL here
-    const response = await fetch('http://localhost:8080/products/' + id);
+    const response = await fetch('/products/' + id);
     const data = await response.json();
     resolve({ data });
   });
@@ -11,7 +11,7 @@ export function fetchProductById(id) {
 export function createProduct(product) {
   
   return new Promise(async (resolve) => {
-    const response = await fetch('http://localhost:8080/products/', {
+    const response = await fetch('/products/', {
       method: 'POST',
       body: JSON.stringify(product),
       headers: { 'content-type': 'application/json' },
@@ -24,7 +24,7 @@ export function createProduct(product) {
 export function updateProduct(update) {
   return new Promise(async (resolve) => {
     const response = await fetch(
-      'http://localhost:8080/products/' + update.id,
+      '/products/' + update.id,
       {
         method: 'PATCH',
         body: JSON.stringify(update),
@@ -40,7 +40,7 @@ export function updateProduct(update) {
 export async function updateRating(update) {
   try {
     const response = await fetch(
-      `http://localhost:8080/products/rating/${update.id}`,
+      `/products/rating/${update.id}`,
       {
         method: 'PATCH',
         body: JSON.stringify({ rating: update.rating }), // Send the updated rating
@@ -89,7 +89,7 @@ export function fetchProductsByFilters(filter, sort, pagination, admin) {
   return new Promise(async (resolve) => {
     //TODO: we will not hard-code server URL here
     const response = await fetch(
-      'http://localhost:8080/products?' + queryString
+      '/products?' + queryString
     );
     const data = await response.json();
     const totalItems = await response.headers.get('X-Total-Count');
@@ -99,7 +99,7 @@ export function fetchProductsByFilters(filter, sort, pagination, admin) {
 
 export function fetchCategories() {
   return new Promise(async (resolve) => {
-    const response = await fetch('http://localhost:8080/categories');
+    const response = await fetch('/categories');
     const data = await response.json();
     resolve({ data });
   });
@@ -107,7 +107,7 @@ export function fetchCategories() {
 
 export function fetchBrands() {
   return new Promise(async (resolve) => {
-    const response = await fetch('http://localhost:8080/brands');
+    const response = await fetch('/brands');
     const data = await response.json();
     resolve({ data });
   });
